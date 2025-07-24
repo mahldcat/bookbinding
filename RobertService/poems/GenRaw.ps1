96ffbd15ca83ad4ea80977543926061b9cd5e978
mahldcat <mahldcat@gmail.com>
2025-07-23 23:35:30 -0700

    adding raw text from gutenberg

--- RobertService/poems/GenRaw.ps1
+++ RobertService/poems/GenRaw.ps1
@@ -14,12 +14,12 @@ write-output "\input{poems/BalladsOfACheechako/to_the_man_of_the_high_north}" >.
 write-output "\newpage" >>.\cheechako.tex
 
 foreach($t in $titles){
-    $tName = "$($t.Replace(" ","_"))"
-    $starter = $template.Replace("%%TITLE%%",$t)
+$tName = "$($t.Replace(" ","_"))"
+$starter = $template.Replace("%%TITLE%%",$t)
 
-    Write-Output "\input{poems/BalladsOfACheechako/$tName}" >>.\cheechako.tex
-    write-output "\newpage" >>.\cheechako.tex
+Write-Output "\input{poems/BalladsOfACheechako/$tName}" >>.\cheechako.tex
+write-output "\newpage" >>.\cheechako.tex
 
-    Write-Output $starter >"./BalladsOfACheechako/$tname.tex"
+Write-Output $starter >"./BalladsOfACheechako/$tname.tex"
 }
 
